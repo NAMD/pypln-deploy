528accf013d45b3b671426bbe125d97824493e50
Flavio Amieiro <amieiro.flavio@gmail.com>
2013-01-10 06:37:19 -0200

    Adds $HOME to the environment when running scripts in virtualenv
    For some reason supervisord is not passing $HOME to this script, and because
    of that we get errors like trying to use "//.python-eggs" as $PYTHON_EGG_CACHE

--- exec_in_virtualenv.sh
+++ exec_in_virtualenv.sh
@@ -19,4 +19,7 @@
 
 
 source /srv/pypln/project/bin/activate
+# For some reason supervisord is not passing $HOME to this script, and because
+# of that we get errors like trying to use "//.python-eggs" as $PYTHON_EGG_CACHE
+export HOME="/srv/pypln"
 exec $*
