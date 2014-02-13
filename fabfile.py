4e401b5866df66c96475cdf82803ef64d7c84499
Flavio Amieiro <amieiro.flavio@gmail.com>
2014-02-13 18:41:46 -0200

    Updates system packages for the wordcloud worker

--- fabfile.py
+++ fabfile.py
@@ -197,7 +197,7 @@ def install_system_packages():
     packages = " ".join(["python-setuptools", "python-pip",
         "python-numpy", "build-essential", "python-dev", "mongodb",
         "pdftohtml", "git-core", "supervisor", "nginx", "python-virtualenv",
-        "postgresql", "python-psycopg2"])
+        "postgresql", "python-psycopg2", "libfreetype6-dev", "fonts-dejavu"])
     sudo("apt-get update")
     sudo("apt-get install -y {}".format(packages))
     # Updating virtualenv is specially important since the default changed
